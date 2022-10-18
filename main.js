2cbf057eadc0737a7d87f909e95c8f8db7fe8f89
MarcoPerta <perta.marco@gmail.com>
2022-10-18 16:05:43 +0200

    prova con il form

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-let username = prompt (`Please enter your name`)
+// let username = prompt (`Please enter your name`)
+// console.log(username)
+
+// let usersurname = prompt (`Please enter your surname`)
+// console.log(usersurname)
+
+// let favoritecolor = prompt (`Please enter your favorite color`)
+// console.log(favoritecolor)
+
+// let number = prompt (`Please enter your number`)
+// console.log(number)
+
+// document.getElementById("h2Password").innerHTML ="Your safest password is " + username+usersurname+favoritecolor+number;
+
+let username = document.getElementById('txt_name_username');
 console.log(username)
 
-let usersurname = prompt (`Please enter your surname`)
+let usersurname = document.getElementById('txt_name_usersurname');
 console.log(usersurname)
 
-let favoritecolor = prompt (`Please enter your favorite color`)
-console.log(favoritecolor)
+let favouritecolor = document.getElementById('txt_name_favouritecolor');
+console.log(favouritecolor)
 
-let number = prompt (`Please enter your number`)
+let number = document.getElementById('txt_name_number');
 console.log(number)
 
 document.getElementById("h2Password").innerHTML ="Your safest password is " + username+usersurname+favoritecolor+number;

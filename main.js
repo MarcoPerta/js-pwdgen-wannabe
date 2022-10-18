cf50d6505523803ed70d4a7c01fe8f07f9dc1c4c
MarcoPerta <perta.marco@gmail.com>
2022-10-18 16:28:43 +0200

    form fallito, back to prompt

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-// let username = prompt (`Please enter your name`)
-// console.log(username)
-
-// let usersurname = prompt (`Please enter your surname`)
-// console.log(usersurname)
-
-// let favoritecolor = prompt (`Please enter your favorite color`)
-// console.log(favoritecolor)
-
-// let number = prompt (`Please enter your number`)
-// console.log(number)
-
-// document.getElementById("h2Password").innerHTML ="Your safest password is " + username+usersurname+favoritecolor+number;
-
-let username = document.getElementById('txt_name_username');
+let username = prompt (`Please enter your name`)
 console.log(username)
 
-let usersurname = document.getElementById('txt_name_usersurname');
+let usersurname = prompt (`Please enter your surname`)
 console.log(usersurname)
 
-let favouritecolor = document.getElementById('txt_name_favouritecolor');
-console.log(favouritecolor)
+let favoritecolor = prompt (`Please enter your favorite color`)
+console.log(favoritecolor)
 
-let number = document.getElementById('txt_name_number');
+let number = prompt (`Please enter your number`)
 console.log(number)
 
 document.getElementById("h2Password").innerHTML ="Your safest password is " + username+usersurname+favoritecolor+number;
+

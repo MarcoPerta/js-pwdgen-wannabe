ad853478f3c0ce80826d381f5e75ff82ddc10810
MarcoPerta <perta.marco@gmail.com>
2022-10-18 15:03:27 +0200

    prompt js

---
+++ main.js
@@ -0,0 +1,11 @@
+let username = prompt (`Please enter your name`)
+console.log(username)
+
+let usersurname = prompt (`Please enter your surname`)
+console.log(usersurname)
+
+let favoritecolor = prompt (`Please enter your favorite color`)
+console.log(favoritecolor)
+
+let number = prompt (`Please enter your number`)
+console.log(number)
